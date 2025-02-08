b3d1d99c6be10741028b6c6f9fdc492c93ec6326
juliangojani <julian@gojani.xyz>
2025-02-08 19:43:28 +0100

    Adding midi visualizer

--- src/composables/useMetronome.ts
+++ src/composables/useMetronome.ts
@@ -30,7 +30,18 @@ export function useMetronome() {
         // Ensure audio context is running
         audioContext.resume();
 
-        const intervalMs = (60 / bpm.value) * 1000;
+
+        let bpmValue = Number(bpm.value);
+        if (bpmValue === 0) {
+            bpmValue = 1
+        }
+        if (bpmValue > 1000) {
+            bpmValue = 1000
+        }
+
+        console.log(bpmValue)
+
+        const intervalMs = (60 / bpmValue) * 1000;
 
         intervalId = setInterval(() => {
             if (!isPlaying.value) return;
